8329dc939e37536635137b968aa1c5e5d8d2ceae
krzyczak <krzyczak@users.noreply.github.com>
2022-05-03 17:14:35 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "/" => "main#index"
+
+  # get "jwt/authentication" => "jwt#jwt_authentication"
+  # get "sso/sign_in_with_saml" => "sso#sign_in_with_saml"
+  post "sso/consume_saml" => "application#consume_saml"
+  # get "sso/sign_out_user" => "sso#sign_out_user"
+  # post "sso/log_out" => "sso#log_out"
+  # get "sso/metadata" => "sso#metadata"
+  # get "sso/sign_in_as" => "sso#sign_in_as"
+end
